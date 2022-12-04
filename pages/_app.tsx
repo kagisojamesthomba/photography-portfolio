c2d4a144672993472e92abb25f419890cb69ec4f
kagiso <kagisojamesthomba@gmail.com>
2022-12-04 13:59:06 +0200

    improve style, add images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import {Aldrich} from '@next/font/google';
+
+const aldrich = Aldrich({
+  subsets: ['latin'],
+  variable: '--font-aldrich',
+  weight: '400',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${aldrich.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
